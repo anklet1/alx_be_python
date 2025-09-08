3ae80afdd1a03b4fdf9791a66aaba25bf60911c2
Felix Fleku <flekufelix@gmail.com>
2025-09-08 16:43:47 +0000

    simple interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,15 @@
+#declaring and assigning of values to variables for a simple interest project
+
+#( I ) is the interest earned,
+#( P ) is the principal amount (initial investment),
+#( R ) is the annual interest rate (as a decimal),
+#( T ) is the time the money is invested for in years.
+
+principal = 1000
+rate = 0.05
+time = 3
+
+interest = principal * rate * time
+
+print(f"The simple interest is: {interest}")
+
