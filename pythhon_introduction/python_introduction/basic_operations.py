7075f214f27e3f5248b47bdbe4cd442ec44a13c6
Felix Fleku <flekufelix@gmail.com>
2025-09-08 16:16:22 +0000

    basic operation on python

---
+++ pythhon_introduction/python_introduction/basic_operations.py
@@ -0,0 +1,16 @@
+#Assigning values to  variables
+
+number1 =  10
+number2 = 5
+
+
+#performing arethmetics 
+sum = number1 + number2
+difference_result = number1 - number2
+product_result = number1 * number2
+
+#print out outputs
+print(f"Adding {number1} and {number2} is {sum}")
+print(f"Subtracting {number1} and {number2} is {difference_result}")
+print(f"Multiplication of {number1} and {number2} is {product_result}")
+
