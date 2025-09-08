b8fbacc457e07e90fd8dc91f74ffcc4a29968e6c
Felix Fleku <flekufelix@gmail.com>
2025-09-08 17:03:10 +0000

    age calculation

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,22 @@
+#Age calculation
+
+#Taking input from user
+
+current_age = int(input("How old are you: "))
+
+
+#Calculate age by 2050
+
+age_by_2050 = 2050 -2023
+future_age = current_age + age_by_2050
+
+
+#print final age
+
+print(f"In 2050, you will be {future_age} years old. ")
+
+
+
+
+
+
